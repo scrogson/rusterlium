816727015b066e945b17d45fa5e919d4da1707f6
Sonny Scroggin <sonny@scrogg.in>
2018-12-28 11:43:54 -0600

    some progress

--- rusterlium/src/lib.rs
+++ rusterlium/src/lib.rs
@@ -1,14 +1,15 @@
-#![allow(dead_code)]
-#[macro_use]
-extern crate rusterlium_codegen;
+pub use rusterlium_codegen::rusterlium;
 
 #[cfg(test)]
 mod test {
+    #![allow(dead_code)]
+    use crate::*;
+
     #[test]
     fn test_nif_macro() {
-        #[nif(hello, 1)]
-        fn hello(env: String) -> String {
-            format!("Hello {}", env)
+        #[rusterlium(name = "add", schedule = "DirtyCpu")]
+        fn add(a: u32, b: u32) -> u32 {
+            a + b
         }
     }
 }
--- rusterlium_codegen/src/lib.rs
+++ rusterlium_codegen/src/lib.rs
@@ -2,24 +2,70 @@ extern crate proc_macro;
 extern crate quote;
 extern crate syn;
 
-//use quote::quote;
 use proc_macro::TokenStream;
-use syn::ItemFn;
+use quote::quote;
+use syn::{
+    parse_macro_input, parse_quote, punctuated::Punctuated, token::Comma, ArgCaptured,
+    AttributeArgs, FnArg, ItemFn,
+};
 
 #[proc_macro_attribute]
-pub fn nif_init(attrs: TokenStream, input: TokenStream) -> TokenStream {
+pub fn rusterlium(args: TokenStream, input: TokenStream) -> TokenStream {
+    let args = parse_macro_input!(args as AttributeArgs);
+    let input = parse_macro_input!(input as ItemFn);
 
-    input
+    let name = extract_attr_value(&args, "name").unwrap_or_else(|| input.ident.to_string());
+    let flags = extract_attr_value(&args, "schedule").unwrap_or_else(|| "Normal".to_string());
+
+    println!("{:?}", name);
+    println!("{:?}", flags);
+
+    let inputs = extract_inputs(&input.decl.inputs)
+        .iter()
+        .enumerate()
+        .map(|(i, (pat, ty))| {
+            parse_quote! {
+                let #pat: #ty = args[#i].decode(env)?;
+            }
+        })
+        .collect::<Vec<syn::Stmt>>();
+
+    println!("{:#?}", inputs);
+
+    let quoted = quote! {
+        #input
+    };
+
+    println!("{}", quoted.to_string());
+
+    quoted.into()
 }
 
-#[proc_macro_attribute]
-pub fn nif(attrs: TokenStream, input: TokenStream) -> TokenStream {
-    let output = input.clone();
-    //let attrs_item = syn::parse(attrs).expect("nif_fn failed to parse");
-    let input_item: ItemFn = syn::parse(input).expect("nif_fn failed to parse");
+fn extract_attr_value<'a>(input: &'a AttributeArgs, attr: &str) -> Option<String> {
+    use syn::{Lit, Meta, MetaNameValue, NestedMeta};
 
-    println!("{:#?}", attrs);
-    //println!("{:#?}", input_item);
+    for item in input.iter() {
+        if let NestedMeta::Meta(Meta::NameValue(meta_name_value)) = item {
+            let MetaNameValue { ident, lit, .. } = meta_name_value;
+            if ident == attr {
+                if let Lit::Str(lit) = lit {
+                    return Some(lit.value());
+                }
+            }
+        }
+    }
 
-    output
+    None
+}
+
+fn extract_inputs<'a>(inputs: &'a Punctuated<FnArg, Comma>) -> Vec<(syn::Pat, syn::Type)> {
+    let mut results = Vec::new();
+
+    for item in inputs.iter() {
+        if let FnArg::Captured(ArgCaptured { pat, ty, .. }) = item {
+            results.push((pat.clone(), ty.clone()));
+        }
+    }
+
+    results
 }
